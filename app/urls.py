17c19a44446dfd9b7296ffca987d3a3a844b1490
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-03-28 11:23:54 +0200

    done with category links filtering in the index, added one pic to the images, completed the add pic functionality, busy with search functionality

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add_photo/', views.add_photo, name='add_photo'),
     path('detailed_view/<str:pk>/', views.detailed_view, name='detailed_view'),
     path('about/', views.about, name='about'),
+    path('search/', views.search, name='search'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,58 +1,53 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Category, Photo, About
 
 
 def home(request):
+    category = request.GET.get("category")  # get the category from the request
+    if category is None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
     categories = Category.objects.all()
-    photos = Photo.objects.all()
-    context = {"categories": categories, "photos": photos}
+
+    context = {
+        "categories": categories,
+        "photos": photos,
+        "category": category,
+    }
     return render(request, "index.html", context)
 
 
 def add_photo(request):
-    user = request.user
-
     categories = Category.objects.all()
 
     if request.method == "POST":
         data = request.POST
-        images = request.FILES.getlist("images")
-
+        images = request.FILES.get("images")
         if data["category"] != "none":
             category = Category.objects.get(id=data["category"])
         elif data["category_new"] != "":
             category, created = Category.objects.get_or_create(
-                user=user, name=data["category_new"]
+                name=data["category_new"]
             )
         else:
             category = None
+        photo = Photo.objects.create(
+            category=category,
+            description=data["description"],
+            pic=images,
+        )
+        return redirect("home")
+    context = {"categories": categories}
 
-        for image in images:
-            photo = Photo.objects.create(
-                category=category,
-                description=data["description"],
-                image=image,
-            )
+    return render(request, "add_photo.html", context)
 
-            return redirect("home")
-        context = {"categories": categories, "photo": photo}
-        return render(request, "add.html", context)
-    # , "categories": categories
-
-    #  title = request.POST.get("title")
-    #     description = request.POST.get("description")
-    #     image = request.FILES.get_list("image")
-    #     category_id = request.POST.get("category")
-    #     category = Category.objects.get(id=category_id)
-
-    #     photo = Photo.objects.create(
-    #         title=title,
-    #         description=description,
-    #         image=image,
-    #         category=category,
-    #         user=user,
-    #     )
+    # search_query = request.GET.get("search")
+    # if search_query:
+    #     photos = photos.filter(description__icontains=search_query) # filter photos based on search query
+    # for pagination
+    # page_number = request.GET.get("page")
+    # paginator = Paginator(photos, 10)  # Show 10 photos per page
 
 
 def detailed_view(request, pk):
@@ -63,3 +58,14 @@ def detailed_view(request, pk):
 
 def about(request):
     return render(request, "about.html")
+
+
+def search(request):
+    # for search query
+    # Check if the request is a post request.
+    if request.method == "GET":
+        # Retrieve the search query entered by the user
+        search_query = request.Get.get["search_query"]
+        # Filter your model by the search query
+        posts = Photo.objects.filter(title__icontains=search_query)
+    return render(request, "search.html", {"query": posts, "posts": posts})
