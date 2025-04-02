c1cba1f27600b8750d4cc4fa21e2888c52dc68b8
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-02 20:42:09 +0200

    migrated from sqlite to postgres

--- main/settings/base.py
+++ main/settings/base.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",  # Database name
+        "USER": "postgres",           # Database username
+        "PASSWORD": "shay12",       # Database password
+        "HOST": "localhost",        # Database host
+        "PORT": "5432",
     }
 }
 
